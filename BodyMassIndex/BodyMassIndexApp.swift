228c23df1cebbe291fd53c14bc2252095a0d9813
Adryan Eka Vandra <adryanekavandra@gmail.com>
2023-03-18 22:17:55 +0700

    Initial Commit

---
+++ BodyMassIndex/BodyMassIndexApp.swift
@@ -0,0 +1,17 @@
+//
+//  BodyMassIndexApp.swift
+//  BodyMassIndex
+//
+//  Created by Adryan Eka Vandra on 18/03/23.
+//
+
+import SwiftUI
+
+@main
+struct BodyMassIndexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
